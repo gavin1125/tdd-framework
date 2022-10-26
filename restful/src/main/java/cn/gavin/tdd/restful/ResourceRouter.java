210487cf21c5f6ea2d8081555246ba441bab0623
chenlh <13692238295@163.com>
2022-10-26 23:36:29 +0800

    sub locator

--- restful/src/main/java/cn/gavin/tdd/restful/ResourceRouter.java
+++ restful/src/main/java/cn/gavin/tdd/restful/ResourceRouter.java
@@ -3,23 +3,16 @@ package cn.gavin.tdd.restful;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.lang.reflect.Type;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static cn.gavin.tdd.restful.DefaultResourceMethod.ValueConverter.singeValued;
 import static java.util.Arrays.stream;
 
 interface ResourceRouter {
@@ -85,98 +78,13 @@ class DefaultResourceMethod implements ResourceRouter.ResourceMethod {
 
     @Override
     public GenericEntity<?> call(ResourceContext resourceContext, UriInfoBuilder builder) {
-        try {
-            UriInfo uriInfo = builder.createUriInfo();
-
-            Object result = method.invoke(builder.getLastMatchedResource(),
-                    stream(method.getParameters()).map(parameter ->
-                            injectParameters(parameter, uriInfo)
-                                    .or(()->injectContext(parameter,resourceContext,uriInfo))
-                                    .orElse(null)).toArray(Object[]::new));
-
-            return result != null ? new GenericEntity<>(result, method.getGenericReturnType()) : null;
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Optional<Object> injectContext(Parameter parameter,ResourceContext resourceContext,UriInfo uriInfo){
-        if(parameter.getType().equals(ResourceContext.class)) return Optional.of(resourceContext);
-        if(parameter.getType().equals(UriInfo.class)) return Optional.of(uriInfo);
-        return Optional.of(resourceContext.getResource(parameter.getType()));
-    }
-
-    private static Optional<Object> injectParameters(Parameter parameter, UriInfo uriInfo) {
-        return providers.stream().map(provider -> provider.provide(parameter, uriInfo)).filter(Optional::isPresent)
-                .findFirst()
-                .flatMap(values -> values.flatMap(v -> convert(parameter, v)));
-    }
-
-    private static Optional<Object> convert(Parameter parameter, List<String> values) {
-        return PrimitiveConverter.convert(parameter, values)
-                .or(() -> ConverterConstructor.convert(parameter.getType(), values.get(0)))
-                .or(() -> ConverterFactory.convert(parameter.getType(), values.get(0)));
-    }
-
-    private static ValueProvider pathParam = (parameter, uriInfo) -> Optional.ofNullable(parameter.getAnnotation(PathParam.class)).map(annotation -> uriInfo.getPathParameters().get(annotation.value()));
-
-    private static ValueProvider queryParam = (parameter, uriInfo) -> Optional.ofNullable(parameter.getAnnotation(QueryParam.class)).map(annotation -> uriInfo.getQueryParameters().get(annotation.value()));
-
-    private static List<ValueProvider> providers = List.of(pathParam, queryParam);
-
-    interface ValueProvider {
-        Optional<List<String>> provide(Parameter parameter, UriInfo uriInfo);
-    }
-
-    interface ValueConverter<T> {
-        T fromString(List<String> values);
-
-        static <T> ValueConverter<T> singeValued(Function<String, T> converter) {
-            return values -> converter.apply(values.get(0));
-        }
+        Object result = MethodInvoker.invoke(method, resourceContext, builder);
+        return result != null ? new GenericEntity<>(result, method.getGenericReturnType()) : null;
     }
 
     @Override
     public String toString() {
         return method.getDeclaringClass().getSimpleName() + "." + method.getName();
-    }
-}
-
-class PrimitiveConverter {
-    private static Map<Type, DefaultResourceMethod.ValueConverter<Object>> primitives = Map.of(
-            int.class, singeValued(Integer::parseInt),
-            String.class, singeValued(s -> s),
-            double.class, singeValued(Double::parseDouble),
-            float.class, singeValued(Float::parseFloat),
-            short.class, singeValued(Short::parseShort),
-            byte.class, singeValued(Byte::parseByte),
-            boolean.class, singeValued(Boolean::parseBoolean)
-    );
-
-    public static Optional<Object> convert(Parameter parameter, List<String> values) {
-        return Optional.ofNullable(primitives.get(parameter.getType()))
-                .map(c -> c.fromString(values));
-    }
-}
-
-class ConverterConstructor {
-    public static Optional<Object> convert(Class<?> converter, String value) {
-        try {
-            return Optional.ofNullable(converter.getConstructor(String.class).newInstance(value));
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException e) {
-            return Optional.empty();
-        }
-    }
-}
-
-class ConverterFactory {
-    public static Optional<Object> convert(Class<?> converter, String value) {
-        try {
-            return Optional.of(converter.getMethod("valueOf", String.class).invoke(null, value));
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            return Optional.empty();
-        }
     }
 }
 
@@ -297,9 +205,8 @@ class SubResourceLocators {
 
         @Override
         public Optional<ResourceRouter.ResourceMethod> match(UriTemplate.MatchResult result, String httpMethod, String[] mediaTypes, ResourceContext resourceContext, UriInfoBuilder builder) {
-            Object resource = builder.getLastMatchedResource();
             try {
-                Object subResource = method.invoke(resource);
+                Object subResource = MethodInvoker.invoke(method, resourceContext, builder);
                 return new ResourceHandler(subResource, uriTemplate).match(result, httpMethod, mediaTypes, resourceContext, builder);
             } catch (Exception e) {
                 throw new RuntimeException(e);
--- restful/src/test/java/cn/gavin/tdd/restful/SubResourceLocatorsTest.java
+++ restful/src/test/java/cn/gavin/tdd/restful/SubResourceLocatorsTest.java
@@ -74,5 +74,4 @@ public class SubResourceLocatorsTest {
             return "content";
         }
     }
-
 }
